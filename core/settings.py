31948d3ab73cc386bec44ccccc20937c3ee5db29
Mohammad Abdul Ahad <maahad.767@gmail.com>
2021-05-06 00:46:31 +0600

    changed debugging mode

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 import django_heroku
 from pathlib import Path
 
@@ -142,3 +143,8 @@ LOGIN_URL = 'login'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+else:
+    DEBUG = False
